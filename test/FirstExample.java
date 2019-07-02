d516a5bc44b22d2391db041d8caf0ba43a7a2815
junius-murphy-integral <junius.murphy@integral.io>
2019-07-02 13:52:00 -0400

    first commit

---
+++ test/FirstExample.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstExample {
+    @Test
+
+    public void firstTest(){
+        assertEquals(2+2 , 3);
+    }
+
+}
