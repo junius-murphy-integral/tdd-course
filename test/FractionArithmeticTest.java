5ec046583e7c26a2c3bc771e72aaceea341bb117
junius-murphy-integral <junius.murphy@integral.io>
2019-07-03 11:34:46 -0400

    update FractionArithmeticTest format

--- test/FractionArithmeticTest.java
+++ test/FractionArithmeticTest.java
@@ -4,21 +4,27 @@ import static org.junit.Assert.assertTrue;
 
 public class FractionArithmeticTest {
     @Test
-    public void addFraction_fractionAdditionEqualsTwo_true() {
+    public void addFraction_fractionAdditionEqualsTwo()
+    {
         Fraction fraction1 = new Fraction(4, 4);
         Fraction fraction2 = new Fraction(4, 4);
-        Fraction result = new Fraction(2, 1);
-        assertTrue(FractionArithmeticUtility.addFraction(fraction1, fraction2).equals(result));
+        Fraction expectedResult = new Fraction(2, 1);
+        Fraction result = FractionArithmeticUtility.addFraction(fraction1, fraction2);
+        assertTrue("Expected: " + expectedResult.numerator + "/" + expectedResult.denominator + "\n" +
+                        "Actual: " + result.numerator + "/" + result.denominator
+                , result.equals(expectedResult));
     }
 
     @Test
-    public void addFraction_ZeroPlusZero_true() {
+    public void addFraction_zeroPlusZero()
+    {
         Fraction fraction1 = new Fraction(0, 10);
         Fraction fraction2 = new Fraction(0, 9);
-        Fraction result = new Fraction(0, 1);
-        assertTrue("Expected: " + result.numerator + "/" + result.denominator + "\n" + "Actual: " + FractionArithmeticUtility.addFraction(fraction1, fraction2).numerator + "/" +
-                        FractionArithmeticUtility.addFraction(fraction1, fraction2).denominator
-                , FractionArithmeticUtility.addFraction(fraction1, fraction2).equals(result));
+        Fraction expectedResult = new Fraction(0, 1);
+        Fraction result = FractionArithmeticUtility.addFraction(fraction1, fraction2);
+        assertTrue("Expected: " + expectedResult.numerator + "/" + expectedResult.denominator + "\n" +
+                        "Actual: " + result.numerator + "/" + result.denominator
+                , result.equals(expectedResult));
 
     }
 }
